413df89a054ca73df4769762d5f2cc67a4d1bcfd
rinsang2000 <rinsang2000@gmai.com>
2022-04-06 10:16:03 +0700

    BT CustomView

---
+++ Sang.Dinh.V/Sang.Dinh.V/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Sang.Dinh.V
+//
+//  Created by Rin Sang on 05/04/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController =  BT5()
+        return true
+    }
+
+ 
+
+}
+
